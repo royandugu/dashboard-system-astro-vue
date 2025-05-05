b54fadfcb8c69931d0e45ddf74e224a1f3d6434e
Royan Dugu (OMG) <royan.dugu@omnicommediagroup.com>
2025-05-05 16:00:54 +0545

    login system

--- src/vue/components/VDashboard.vue
+++ src/vue/components/VDashboard.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref } from 'vue';
 
+const pageShowStatus = ref(0); //0 = loading 1=show next will be redirect on 0
 const allUsers = ref([]);
 const popUpToShow = ref(false);
-const isLoading = ref(false);
+const selectedId = ref(null);
+const loadingValues = ref(0); //0 = neutral 1 = loading 2=error 
 
 const fetchAllUsers = async () => {
   const response = await fetch("http://localhost:3001/users");
@@ -17,24 +19,40 @@ const showPopup = () => {
 const hidePopup = () => {
   popUpToShow.value = false;
 }
+const setSelectedId = (id) =>{
+  selectedId.value=id;
+}
 
-const deleteUser = async (id) =>{
-  const response = await fetch(`http://localhost:3001/users/${id}`,{
+const deleteUser = async () =>{
+  loadingValues.value=1;
+  const response = await fetch(`http://localhost:3001/users/${selectedId.value}`,{
     method:"DELETE"
   });
-  
+  if(response.status===200){ 
+    await fetchAllUsers();
+    popUpToShow.value=false;
+    loadingValues.value=0;
+  }
+  else{
+    loadingValues.value=2;
+  }
 }
 
 fetchAllUsers();
 </script>
+
 <template>
+  <div v-if="pageShowStatus === 0"> </div>
   <div>
     <h1>Welcome to dashboard -- The list of users are : </h1>
     <ul>
       <li class="indv-user-space" v-for="(user, index) in allUsers" :key="index">
         <p> Name : {{ user.username }} </p>
         <p> Rollnumber : {{ user.rollNumber }} </p>
-        <button @click="showPopup"> Delete user </button>
+        <button @click="()=>{
+          showPopup();
+          setSelectedId(user.id);
+        }"> Delete user </button>
       </li>
     </ul>
   </div>
@@ -42,7 +60,15 @@ fetchAllUsers();
   <div v-if="popUpToShow" class="pop-up-wrapper">
     <h5> Do you really want to delete this user ? </h5>
     <div class="pop-up-btn-container">
-      <button> Yes </button>
+      <div v-if="loadingValues === 0">
+        <button @click="()=>deleteUser()"> Yes </button>
+      </div>
+      <div v-else-if="loadingValues === 1">
+        <button class="loading-button"> Deleting user .. </button>
+      </div>
+      <div v-else-if="loadingValues === 2">
+        <button> Error, try again .. </button>  
+      </div>
       <button @click="hidePopup"> No </button>
     </div>
   </div>
@@ -83,5 +109,9 @@ button {
   display: flex;
   justify-content: center;
   gap: 20px;
+}
+.loading-button{
+  opacity: .5;
+  pointer-events: none;
 }
 </style>
--- src/vue/components/VLogin.vue
+++ src/vue/components/VLogin.vue
@@ -1,7 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref("");
+const password = ref("");
+
+const usernameError = ref(false);
+const passwordError = ref(false);
+
+const onUsernameEntered = (e) => {
+    username.value = e.target.value;
+}
+const onPasswordEntered = (e) => {
+    password.value = e.target.value;
+}
+
+const loginUser = async (e) => {
+    e.preventDefault();
+    let proceedFurther = true;
+    if (username.value === "" || username.value === " ") {
+        usernameError.value = true;
+        passwordError = false;
+    }
+    if (password.value === "" || password.value === " ") {
+        passwordError.value = true;
+        proceedFurther = false;
+    }
+    if (proceedFurther) {
+        const result = await fetch("http://localhost:3001/users");
+        const allData = await result.json();
+    }
+} 
+</script>
 <template>
-    <form>
-        <input type="text" placeholder="Username"/><br/>
-        <input type="password" placeholder="Password"/><br/>
+    <form @submit="loginUser">
+        <input type="text" placeholder="Username" @input="onUsernameEntered" :class="{'red-border':usernameError}"/><br />
+        <input type="password" placeholder="Password" @input="onPasswordEntered" :class="{'red-border':passwordError}"/><br />
         <button type="submit"> Login </button>
     </form>
 </template>
